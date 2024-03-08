0040a20f62b212937f7151c204afdaa9dac2dd6a
funi504 <nekhungunifunanani9@gmail.com>
2024-03-08 14:03:56 +0200

    reviewed the code

--- action.js
+++ action.js
@@ -98,47 +98,4 @@ const reply = (replyId, name) => {
 
 
 
-    //do get data from database
-    /*replyDummyData.map((action)=>{
-
-        console.log(action.Id)
-        let actionId = action.Id;
-
-        if(actionId === Id){
-            let userText = '<p class="userText"><span>' + action.actionName + "</span></p>";
-            $("#chatbox").append(userText);
-
-            $("#chatbox").append('<p class="botText"><span>' + action.reply + "</span></p>");
-
-            action.nextActionChoices.map(( nextAction)=>{
-
-                $("#chatbox").append(nextAction);
-                //$("#action-list").append('<div class = "action">just got appended </div>')
- 
-            })
-            
-        }
-    })
-
-    //let botText = '<p class="botText"><span>' + response + "</span></p>";
-    //let nextActionChooices = '<div class = "action"> Book a call </div>'
-    //$("#chatbox").append(botText);
-    //$("#chatbox").append(nextActionChooices);
-
-}
-
-// action button functions
-const emailActionBtn = ()=>{
-
-    document.getElementById("form-container").style.display = "block";
-}
-
-
-// reply dummy data 
-const buttonData = [
-   {
-        name:"services",
-        Id: "123",
-        reply_id : "123replyid" 
-    },
-] */ 
+  
